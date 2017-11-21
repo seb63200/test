95861f81a66c8fa929d13cffd8adb33a3561ef5f
seb63200 <32297915+seb63200@users.noreply.github.com>
2017-11-21 15:13:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-module.exports = function() {
-    var exec = require('./exec.js');
-	var setup = require('./setup.js);
+module.exports = function(sails) {
+	
+    var exec = require('./lib/exec.js');
+    var setup = require('./setup.js);
 
     return {
-        exec: exec
-		setup: setup		
+        exec: exec,
+	setup: setup		
     };
 };
